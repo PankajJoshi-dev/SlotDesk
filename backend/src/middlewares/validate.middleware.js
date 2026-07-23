509e1b86fbb2162ef99acd91eae74e5aa1390e14
Pankaj <pj5283066@gmail.com>
2026-07-23 19:07:11 +0530

    Enhance validation middleware to support dynamic source for request data

--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -1,15 +1,21 @@
 import apiError from "../utils/ApiError.js";
 
-export const validate = (schema) => {
+export const validate = (schema, source = "body") => {
   return (req, res, next) => {
-    const result = schema.safeParse(req.body);
+    const result = schema.safeParse(req[source]);
 
     if (!result.success) {
       const message = result.error.issues[0].message;
       return next(new apiError(400, message));
     }
 
-    req.validatedData = result.data;
+    if (source === "query") {
+      req.validatedQuery = result.data;
+    } else if (source === "params") {
+      req.validatedParams = result.data;
+    } else {
+      req.validatedBody = result.data;
+    }
     next();
   };
 };
