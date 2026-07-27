2f6024531798da2e05c59aa5cc71721b080cd207
Pankaj <pj5283066@gmail.com>
2026-07-27 17:43:28 +0530

    Implement Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>This is Navbar.</div>;
+  return (
+    <nav className="w-full fixed top-0 left-0 z-50 h-16 px-6 bg-surface  flex flex-row justify-between items-center  shadow-md">
+      <Link to="/" className="flex items-center">
+        <img src={logo} alt="SlotDesk Logo" className="h-14 w-auto" />
+      </Link>
+      <div className="flex flex-row justify-end gap-4">
+        <Link
+          to={"/register"}
+          className="bg-primary transition-colors hover:bg-primary-hover px-3 py-2 rounded-sm font-semibold"
+        >
+          Register
+        </Link>
+        <Link
+          to={"/Login"}
+          className="bg-primary transition-colors hover:bg-primary-hover px-3 py-2 rounded-sm font-semibold"
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
