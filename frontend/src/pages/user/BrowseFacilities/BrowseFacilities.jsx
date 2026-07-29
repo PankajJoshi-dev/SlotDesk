7c69da55d4f6b61a2b9455adaf05cc9004c945b4
Pankaj <pj5283066@gmail.com>
2026-07-29 20:59:54 +0530

    Implement BrowseFacilities page with facility-cards and filtering options

--- frontend/src/pages/user/BrowseFacilities/BrowseFacilities.jsx
+++ frontend/src/pages/user/BrowseFacilities/BrowseFacilities.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearch } from "../../../contexts/SearchContext";
+import FilterSection from "./components/FilterSection";
+import FacilityCard from "./components/FacilityCard";
 
 function BrowseFacilities() {
-  return <div>This is BrowseFacilities page.</div>;
+  const { facilities } = useSearch();
+
+  const facilityCards = facilities.map((facility) => (
+    <FacilityCard key={facility._id} facility={facility} />
+  ));
+
+  return (
+    <>
+      <FilterSection />
+      <div className="flex flex-row justify-center scroll-auto"></div>
+      <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4 p-4">
+        {facilityCards}
+      </div>
+    </>
+  );
 }
 
 export default BrowseFacilities;
