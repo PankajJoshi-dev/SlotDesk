2de346743f42122545ca78a7b0619754ac3d9379
Pankaj <pj5283066@gmail.com>
2026-07-29 21:04:57 +0530

    Refactor Mainlayout and update Navbar and Footer paths

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <main className="pt-16 flex-1">
+      <main className="flex-1 pt-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
         <Outlet />
       </main>
 
