14a0d51be6dafff7c405a784e4678167b8fc598b
Pankaj <pj5283066@gmail.com>
2026-08-01 23:12:33 +0530

    Add partySize and Booking button to BookSlot component

--- frontend/src/pages/user/FacilityBooking/components/BookSlot.jsx
+++ frontend/src/pages/user/FacilityBooking/components/BookSlot.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import DateSelector from "./DateSelector";
+import PartySize from "./PartySize";
 import SlotSelector from "./SlotSelector";
+import { useBooking } from "../../../../contexts/BookingContext";
+import { useFacility } from "../../../../contexts/FacilityContext";
 
 function BookSlot() {
+  const { bookFacility } = useBooking();
+
   return (
     <div className="w-full mx-auto p-6 space-y-6">
       <DateSelector />
+      <PartySize />
       <SlotSelector />
+      <div className="pt-6 mt-6 border-t border-border flex justify-end">
+        <button
+          className="w-full sm:w-auto bg-primary hover:bg-primary-hover transition-all duration-200 px-8 py-3 rounded-md font-semibold shadow hover:shadow-lg"
+          onClick={() => bookFacility()}
+        >
+          Book Now
+        </button>
+      </div>
     </div>
   );
 }
