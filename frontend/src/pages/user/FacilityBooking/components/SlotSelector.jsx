b12951f539ed5ea7e2e1267706698ed2326f01bf
Pankaj <pj5283066@gmail.com>
2026-08-02 21:33:08 +0530

    Enhance SlotSelector to manage loading state and improve error handling for slot selection

--- frontend/src/pages/user/FacilityBooking/components/SlotSelector.jsx
+++ frontend/src/pages/user/FacilityBooking/components/SlotSelector.jsx
@@ -4,7 +4,15 @@ import { useBooking } from "../../../../contexts/BookingContext";
 
 function SlotSelector() {
   const { facilityDetails, slots } = useFacility();
-  const { slotIndex, setSlotIndex } = useBooking();
+  const { slotIndex, setSlotIndex, errors, loading } = useBooking();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-[70vh]">
+        Loading...
+      </div>
+    );
+  }
 
   const slotCards = slots.map((slot, i) => {
     const startTime = Number(
@@ -17,24 +25,44 @@ function SlotSelector() {
     const period = hours >= 12 ? "PM" : "AM";
     hours = String(hours % 12).padStart(2, "0");
 
+    const isAvailable = slot.isAvailable;
+
     const isSelected = slotIndex === i;
 
     return (
       <button
         key={i}
+        disabled={!isAvailable}
         onClick={() => setSlotIndex(i)}
-        className={`shrink-0 rounded-xl border p-3 transition-all cursor-pointer ${isSelected ? "bg-primary/20 border-primary" : "bg-card hover:bg-card/80"}`}
+        className={`shrink-0 rounded-xl border p-3 text-xs font-semibold transition-all
+  ${
+    isSelected
+      ? errors?.slotIndex
+        ? "border-red-500 bg-red-500"
+        : "bg-primary/20 text-primary-foreground border-primary"
+      : !isAvailable
+        ? "bg-muted text-muted-foreground border-border opacity-50 cursor-not-allowed"
+        : "bg-card hover:bg-card/80 border-border cursor-pointer"
+  }
+`}
       >
-        {hours} : {mins} {period}
+        {hours}:{mins} {period}
       </button>
     );
   });
 
   return (
-    <>
-      <h1 className="text-2xl font-bold">Select Slot</h1>
-      <div className="w-full grid grid-cols-4 gap-2">{slotCards}</div>
-    </>
+    <div className="space-y-2">
+      <h1 className="text-xl font-semibold">Select Slot</h1>
+
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-2">
+        {slotCards}
+      </div>
+
+      {errors?.slotIndex && (
+        <p className="text-sm text-error">{errors.slotIndex}</p>
+      )}
+    </div>
   );
 }
 
