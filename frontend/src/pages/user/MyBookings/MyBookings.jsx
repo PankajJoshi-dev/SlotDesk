53b7586c6d5973fafdb50bb9ef4f4eae1e70094a
Pankaj <pj5283066@gmail.com>
2026-08-04 16:29:24 +0530

    Implement MyBookings page to display user bookings with BookingCard and BookingFilters

--- frontend/src/pages/user/MyBookings/MyBookings.jsx
+++ frontend/src/pages/user/MyBookings/MyBookings.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import BookingCard from "./components/BookingCard";
+import { useMyBooking } from "../../../contexts/MyBookingContext";
+import BookingFilters from "./components/BokingFilters";
 
-function BookSlot() {
-  return <div>This is BookSlot page.</div>;
+function MyBookings() {
+  const { mybookings, loading } = useMyBooking();
+
+  const bookingCards = mybookings.map((booking) => {
+    return <BookingCard key={booking?._id} booking={booking} />;
+  });
+
+  return (
+    <div>
+      <h1 className="text-xl font-semibold m-4">Your Bookings</h1>
+      <BookingFilters />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-6 mx-4">
+        {bookingCards}
+      </div>
+    </div>
+  );
 }
 
-export default BookSlot;
+export default MyBookings;
