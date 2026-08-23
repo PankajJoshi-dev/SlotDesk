69eee0d3fdf00aa17677b710ec2a5259e8b8b924
Pankaj <pj5283066@gmail.com>
2026-08-23 13:02:27 +0530

    Remove default Content-Type header from axios instance

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,9 +5,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Send cookies
   timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 api.interceptors.response.use(
