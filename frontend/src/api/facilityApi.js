6ab75f10716d733f109e8dfbf02d894a62d5295e
Pankaj <pj5283066@gmail.com>
2026-08-23 12:57:36 +0530

    Refactor facility API requests

--- frontend/src/api/facilityApi.js
+++ frontend/src/api/facilityApi.js
@@ -7,20 +7,51 @@ const log = (data) => {
   }
 };
 
+const registerFacilityRequest = async (facilityData) => {
+  const response = await api.post("/facilities", facilityData);
+  log(response.data);
+  return response.data;
+};
+
+const editFacilityRequest = async (facilityId, facilityData) => {
+  const response = await api.patch(`/facilities/${facilityId}`, facilityData);
+  log(response.data);
+  return response.data;
+};
+
+const deleteFacilityRequest = async (facilityId) => {
+  const response = await api.delete(`/facilities/${facilityId}`);
+  log(response.data);
+  return response.data;
+};
+
 const facilityDetailsRequest = async (facilityId) => {
   const response = await api.get(`/facilities/${facilityId}`);
   log(response.data);
   return response.data;
 };
 
-const geteSlotsRequest = async (facilityId, date) => {
+const getFacilityBookingsRequest = async (facilityId) => {
+  const response = await api.get(`/facilities/${facilityId}/bookings`);
+  log(response.data);
+  return response.data;
+};
+
+const getSlotsRequest = async (facilityId, date) => {
   const response = await api.get(`/facilities/${facilityId}/slots`, {
     params: {
-      date: date,
+      date,
     },
   });
   log(response.data);
   return response.data;
 };
 
-export { facilityDetailsRequest, geteSlotsRequest };
+export {
+  registerFacilityRequest,
+  editFacilityRequest,
+  deleteFacilityRequest,
+  facilityDetailsRequest,
+  getFacilityBookingsRequest,
+  getSlotsRequest,
+};
